18102de3556ff60f6ad3ee1e61b3cf0aff7c1de6
Ritwick Dey <ritwickdey@outlook.com>
2018-02-06 16:50:44 +0530

    webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -15,5 +15,8 @@ module.exports = {
       }
     ]
   },
-  devtool: 'cheap-module-eval-source-map'
+  devtool: "cheap-module-eval-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "public")
+  }
 };
